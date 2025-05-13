3ee4023b098f19a0fe6d36c560133099ef5734fe
FIRST_NAME LAST_NAMEdrew mistry <drewmistry05@gmail.com>
2025-05-13 19:03:53 +0100

    item template

--- item_template.php
+++ item_template.php
@@ -17,29 +17,31 @@
 
   <!-- Section to display item details fetched from the database -->
   <div class="item-details-flex">
-    <div class="item-images-col">
-      <div class="auction-badge" id="auction-badge" style="display:none;">HOT AUCTION</div>
-      <div id="image-carousel">
-        <button class="carousel-arrow carousel-prev"><i class="fas fa-chevron-left"></i></button>
-        <button class="carousel-arrow carousel-next"><i class="fas fa-chevron-right"></i></button>
-        <div class="carousel-nav"></div>
+    <h1 id="item-title" class="item-title-centered"></h1>
+    <div class="item-columns">
+      <div class="item-images-col">
+        <div class="auction-badge" id="auction-badge" style="display:none;">HOT AUCTION</div>
+        <div id="image-carousel">
+          <button class="carousel-arrow carousel-prev"><i class="fas fa-chevron-left"></i></button>
+          <button class="carousel-arrow carousel-next"><i class="fas fa-chevron-right"></i></button>
+          <div class="carousel-nav"></div>
+        </div>
       </div>
-    </div>
-    <div class="item-info-col">
-      <h1 id="item-title"></h1>
-      <div class="seller-info" id="seller-info"></div>
-      <div class="item-price-row">
-        <span id="item-price"></span>
-        <button id="bid-button">Submit bid</button>
+      <div class="item-info-col">
+        <div class="seller-info" id="seller-info"></div>
+        <div class="item-price-row">
+          <span id="item-price"></span>
+          <button id="bid-button">Submit bid</button>
+        </div>
+        <div class="item-meta">
+          <p><strong>Condition:</strong> <span id="item-condition"></span></p>
+          <p><strong>Postage:</strong> <span id="postage"></span></p>
+          <p><strong>Time Remaining:</strong> <span id="countdown-timer"></span></p>
+          <p><strong>Start Date:</strong> <span id="start-date"></span></p>
+          <p><strong>End Date:</strong> <span id="end-date"></span></p>
+        </div>
+        <div class="item-description" id="item-description"></div>
       </div>
-      <div class="item-meta">
-        <p><strong>Condition:</strong> <span id="item-condition"></span></p>
-        <p><strong>Postage:</strong> <span id="postage"></span></p>
-        <p><strong>Time Remaining:</strong> <span id="countdown-timer"></span></p>
-        <p><strong>Start Date:</strong> <span id="start-date"></span></p>
-        <p><strong>End Date:</strong> <span id="end-date"></span></p>
-      </div>
-      <div class="item-description" id="item-description"></div>
     </div>
   </div>
 
